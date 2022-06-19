beaa8b9bd75b3afdfe86eb091c002bd057a4467c
KOTARO SUTO <ng1a5ss43@gmail.com>
2022-06-19 00:44:46 -0700

    Add docs to Script+Files.swift

--- Sources/ScriptSwift/Script+Files.swift
+++ Sources/ScriptSwift/Script+Files.swift
@@ -2,6 +2,8 @@ import struct Foundation.Data
 import Files
 
 extension Script where T == File {
+    /// This function reads file from the value of the previous ``Script`` method.
+    /// - Returns: ``Script`` object with `Data` representation of the file or failure
     public func read() -> Script<Data> {
         switch input {
         case .success(let input):
@@ -15,7 +17,10 @@ extension Script where T == File {
             return .init(failure: error)
         }
     }
-
+    
+    /// This function reads file from the value of the previous ``Script`` method.
+    /// - Parameter encoded: This indicates how the file is encoded.
+    /// - Returns: ``Script`` object with `String` representation of the file or failure
     public func read(encoded: String.Encoding = .utf8) -> Script<String> {
         switch input {
         case .success(let input):
