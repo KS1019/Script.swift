6713bbf2784c2f99440ad4fc1944b0978db3d7bc
KOTARO SUTO <ng1a5ss43@gmail.com>
2024-03-17 15:52:29 -0700

    Renames `number`

--- Sources/Scripting/Script+Comparable.swift
+++ Sources/Scripting/Script+Comparable.swift
@@ -1,37 +1,37 @@
 /// Scripting methods whose values are comparable
 extension Script where T: Comparable {
 
-    /// This method return true when the piped value is more than `number` and false otherwise.
-    /// - Parameter number: `Comparable` value
-    /// - Returns: Bool value by comparing the piped value and the parameter `number`
-    public func more(than number: T) -> Script<Bool> {
-        switch input {
-        case .success(let input):
-            return .init(success: input > number)
+    /// This method return true when the piped value is more than `rhs` and false otherwise.
+    /// - Parameter rhs: `Comparable` value
+    /// - Returns: Bool value by comparing the piped value and the parameter `rhs`
+    public func more(than rhs: T) -> Script<Bool> {
+        switch lhs {
+        case .success(let lhs):
+            return .init(success: lhs > rhs)
         case .failure(let error):
             return .init(failure: error)
         }
     }
 
-    /// This method return true when the piped value is less than `number` and false otherwise.
-    /// - Parameter number: `Comparable` value
-    /// - Returns: Bool value by comparing the piped value and the parameter `number`
-    public func less(than number: T) -> Script<Bool> {
-        switch input {
-        case .success(let input):
-            return .init(success: input < number)
+    /// This method return true when the piped value is less than `rhs` and false otherwise.
+    /// - Parameter rhs: `Comparable` value
+    /// - Returns: Bool value by comparing the piped value and the parameter `rhs`
+    public func less(than rhs: T) -> Script<Bool> {
+        switch lhs {
+        case .success(let lhs):
+            return .init(success: lhs < rhs)
         case .failure(let error):
             return .init(failure: error)
         }
     }
 
-    /// This method return true when the piped value is equal to `number` and false otherwise.
-    /// - Parameter number: `Comparable` value
-    /// - Returns: Bool value by comparing the piped value and the parameter `number`
-    public func equal(to number: T) -> Script<Bool> {
-        switch input {
-        case .success(let input):
-            return .init(success: input == number)
+    /// This method return true when the piped value is equal to `rhs` and false otherwise.
+    /// - Parameter rhs: `Comparable` value
+    /// - Returns: Bool value by comparing the piped value and the parameter `rhs`
+    public func equal(to rhs: T) -> Script<Bool> {
+        switch lhs {
+        case .success(let lhs):
+            return .init(success: lhs == rhs)
         case .failure(let error):
             return .init(failure: error)
         }
